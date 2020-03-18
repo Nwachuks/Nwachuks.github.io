4f3932072ed2f37b610362f0ad4448dea5b262c5
Nwachukwu Ejiofor <nwachukwu.ejiofor@xputertechnologies.com>
2020-03-18 18:16:29 +0100

    Add progress bars

--- js/index.js
+++ js/index.js
@@ -1,10 +1,13 @@
 function slideImageRight() {
-    const elems =  document.getElementsByClassName('work-flex');
+    // Get all work elements
+    const elems = document.getElementsByClassName('work-flex');
+    // Find current visible work element without d-none
     for(let i = 0; i < elems.length; i++) {
         if (!elems[i].classList.contains('d-none')) {
             currentCount = i;
         }
-    }      
+    } 
+    // If not last work element, manipulate element class list to make next element visible and current element hidden
     if (currentCount < elems.length - 1) {
         elems[currentCount + 1].classList.remove('d-none');
         elems[currentCount].classList.add('d-none');
@@ -12,20 +15,23 @@ function slideImageRight() {
 }
 
 function slideImageLeft() {
+    // Get all work elements
     const elems =  document.getElementsByClassName('work-flex');
+    // Get current visible work element without d-none
     for(let i = 0; i < elems.length; i++) {
         if (!elems[i].classList.contains('d-none')) {
             currentCount = i;
         }
     }
+    // If not first work element, make previous element visible and current element hidden
     if (currentCount > 0) {
         elems[currentCount - 1].classList.remove('d-none');
         elems[currentCount].classList.add('d-none');
     }
 }
 
+// Make about section appear when in view
 const elem = document.querySelector('#about');
-
 function isInViewport(elem) {
     const rect = elem.getBoundingClientRect();
     return (
@@ -46,6 +52,7 @@ function run() {
 window.addEventListener('load', run);
 window.addEventListener('scroll', run);
 
+// Smooth out scrolling on page
 $('.btn').on('click', function (event) {
     if (this.hash !== '') {
         event.preventDefault();
