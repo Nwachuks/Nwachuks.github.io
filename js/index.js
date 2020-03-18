3a10aa3cc001b929a001057936f6bbb0a76095b4
Nwachukwu Ejiofor <nwachukwu.ejiofor@xputertechnologies.com>
2020-03-18 14:49:49 +0100

    Replace header image

--- js/index.js
+++ js/index.js
@@ -39,7 +39,7 @@ function isInViewport(elem) {
   
 function run() {
     if (isInViewport(elem)) {
-     elem.classList.add('show');
+        elem.classList.add('show');
     }
 }
 
@@ -48,10 +48,10 @@ window.addEventListener('scroll', run);
 
 $('.btn').on('click', function (event) {
     if (this.hash !== '') {
-      event.preventDefault();
-      const hash = this.hash;
-      $('html, body').animate({
+        event.preventDefault();
+        const hash = this.hash;
+        $('html, body').animate({
         scrollTop: $(hash).offset().top
-      }, 1400);
+        }, 1400);
     }
-})
+});
